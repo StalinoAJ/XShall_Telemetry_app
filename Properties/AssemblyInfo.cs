f456d5cb085642519566c5e27e143140cab14f0d
StalinoAJ <stalinosmj@outlook.com>
2026-07-03 12:58:55 +0530

    ci: add automated build and release workflow with dynamic AssemblyInfo versioning

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SHALL XR Seat Controller")]
 [assembly: AssemblyDescription("Motion seat controller for SHALL XR gaming chair")]
-[assembly: AssemblyVersion("1.1.4.0")]
-[assembly: AssemblyFileVersion("1.1.4.0")]
+[assembly: AssemblyVersion("1.1.6.0")]
+[assembly: AssemblyFileVersion("1.1.6.0")]
 [assembly: ComVisible(false)]
